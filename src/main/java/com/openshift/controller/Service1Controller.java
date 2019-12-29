611ae566d85d7d2aaccba8d1777dc0713551c1fd
Sibsankar Bera <sibsankarb4@gmail.com>
2019-12-29 20:58:14 +0530

    Update Service1Controller.java

--- src/main/java/com/openshift/controller/Service1Controller.java
+++ src/main/java/com/openshift/controller/Service1Controller.java
@@ -3,13 +3,13 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 
 
@@ -22,13 +22,12 @@ public class Service1Controller {
 	
 	@Value("${service2.product-url}")
         private String service2_product_url;
-	//String service2_product_url="https://service-2-sibsber1-23934.osc-sbx-exp-ap-56325c34021cf286d0e188cc291cdca2-0001.us-east.containers.appdomain.cloud/Service-2-v1/service2/products";
+	
 	
 	@GetMapping("/info")
 	public String welcome() {
 		return "++++++++++++++++++++++++++++  -: Welcome to Service 1 :- +++++++++++++++++++++++++++++++++ ";
 	}
-	
 	
 	@RequestMapping(value = "/getProductsList", method = RequestMethod.GET)	
 	public List<Object> getProducts() {	 
@@ -36,8 +35,6 @@ public class Service1Controller {
 	ResponseEntity<Object[]> response = restTemplate.getForEntity(service2_product_url, Object[].class);
 	return Arrays.asList(response.getBody());
 	
-	}
-	
-	
+	}	
 	
 }
